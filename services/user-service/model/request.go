a1d10fe2a2dc22e30b790698cae51baf8c029efd
abhinandpn <abhinandpn4067@gmail.com>
2026-07-06 04:39:32 +0530

    Add example values to fields in RegisterRequest struct

--- services/user-service/model/request.go
+++ services/user-service/model/request.go
@@ -1,8 +1,8 @@
 package model
 
 type RegisterRequest struct {
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Number   string `json:"number"`
-	Password string `json:"password"`
+	Name     string `json:"name" example:"John Doe"`
+	Email    string `json:"email" example:"john@example.com"`
+	Number   string `json:"number" example:"9876543210"`
+	Password string `json:"password" example:"StrongPassword123"`
 }
